648542c5dd7213c89b0e8ddd508e7fb3fa70db40
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-04 14:52:13 +0600

    feat:routing with express.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-import express from "express"
+// import express from "express"
 
-const  app= express()
-const port = 7000;
-app.get("/" , (req,res) =>{
-res.send("Hello Bro")
-})
-app.listen( port , () =>{
-    console.log(`server stated at ${port}`);
+// const  app= express()
+// const port = 7000;
+// app.get("/" , (req,res) =>{
+// res.send("Hello Bro");
+// });
+// app.listen( port , () =>{
+//     console.log(`server stated at ${port}`);
     
-})
+// })
+
+
+// ================ Routing code with express ===========
+ import express from "express"
+
+ const app = express();
+ const port = 7000;
+
+ app.get("/" , (req, res) =>{
+    res.send("This is a home page");
+ });
+ app.get("/about" , (req, res) =>{
+    res.send("Tihs is a about page");
+ });
+ app.get("/contact" , (req, res) =>{
+    res.send("This is a contact page ");
+ });
+ app.listen(port , () =>{
+    console.log(`Server is stated at ${port}`);
+    
+ });
